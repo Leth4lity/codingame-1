a2b7e64af91e12d22c5cc9d9730d24c6182c7570
James Harris <wopian@wopian.me>
2017-11-17 11:22:49 +0000

    chore: remove pointless comments

--- practice/easy/onboarding/javascript.js
+++ practice/easy/onboarding/javascript.js
@@ -1,8 +1,8 @@
 while (true) {
-    var enemy1 = readline(); // name of enemy 1
-    var dist1 = parseInt(readline()); // distance to enemy 1
-    var enemy2 = readline(); // name of enemy 2
-    var dist2 = parseInt(readline()); // distance to enemy 2
+    const enemy1 = readline()
+    const dist1 = parseInt(readline())
+    const enemy2 = readline()
+    const dist2 = parseInt(readline())
 
     if (dist1 < dist2) print(enemy1)
     else print(enemy2)
