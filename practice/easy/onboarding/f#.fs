ec0d4050f01105a2d091ce30cd5afb4769d75253
James Harris <wopian@wopian.me>
2017-11-17 11:21:39 +0000

    chore: remove pointless comments

--- practice/easy/onboarding/f#.fs
+++ practice/easy/onboarding/f#.fs
@@ -1,11 +1,11 @@
 open System
 
 while true do
-    let enemy1 : string = Console.In.ReadLine() (* name of enemy 1 *)
-    let dist1 = int(Console.In.ReadLine()) (* distance to enemy 1 *)
-    let enemy2 : string = Console.In.ReadLine() (* name of enemy 2 *)
-    let dist2 = int(Console.In.ReadLine()) (* distance to enemy 2 *)
-    
-    if dist1 < dist2
-    then printfn "%s" enemy1
-    else printfn "%s" enemy2
+  let enemy1 : string = Console.In.ReadLine()
+  let dist1 = int(Console.In.ReadLine())
+  let enemy2 : string = Console.In.ReadLine()
+  let dist2 = int(Console.In.ReadLine())
+
+  if dist1 < dist2
+  then printfn "%s" enemy1
+  else printfn "%s" enemy2
