8eb57b52ab6630844a1e11482eae5f363efa5d6d
James Harris <wopian@wopian.me>
2017-11-17 11:26:59 +0000

    chore: remove pointless comments

--- practice/easy/onboarding/python3.py
+++ practice/easy/onboarding/python3.py
@@ -2,10 +2,10 @@ import sys
 import math
 
 while True:
-    enemy_1 = input()  # name of enemy 1
-    dist_1 = int(input())  # distance to enemy 1
-    enemy_2 = input()  # name of enemy 2
-    dist_2 = int(input())  # distance to enemy 2
+    enemy_1 = input()
+    dist_1 = int(input())
+    enemy_2 = input()
+    dist_2 = int(input())
 
     if dist_1 < dist_2:
         print(enemy_1)
