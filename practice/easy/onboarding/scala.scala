9136c7cc0bf8b64962a390396356076775d2727b
James Harris <wopian@wopian.me>
2017-11-17 11:28:24 +0000

    chore: remove pointless comments

--- practice/easy/onboarding/scala.scala
+++ practice/easy/onboarding/scala.scala
@@ -3,11 +3,11 @@ import scala.util._
 
 object Player extends App {
     while(true) {
-        val enemy1 = readLine // name of enemy 1
-        val dist1 = readInt // distance to enemy 1
-        val enemy2 = readLine // name of enemy 2
-        val dist2 = readInt // distance to enemy 2
-        
+        val enemy1 = readLine
+        val dist1 = readInt
+        val enemy2 = readLine
+        val dist2 = readInt
+
         if (dist1 < dist2) println(enemy1)
         else println(enemy2)
     }
