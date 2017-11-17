1413926da42307eff1addb8a1e3698942558a29b
James Harris <wopian@wopian.me>
2017-11-17 11:27:57 +0000

    chore: remove pointless comments

--- practice/easy/onboarding/ruby.rb
+++ practice/easy/onboarding/ruby.rb
@@ -1,10 +1,10 @@
-STDOUT.sync = true # DO NOT REMOVE
+STDOUT.sync = true
 
 loop do
-    enemy_1 = gets.chomp # name of enemy 1
-    dist_1 = gets.to_i # distance to enemy 1
-    enemy_2 = gets.chomp # name of enemy 2
-    dist_2 = gets.to_i # distance to enemy 2
-    
+    enemy_1 = gets.chomp
+    dist_1 = gets.to_i
+    enemy_2 = gets.chomp
+    dist_2 = gets.to_i
+
     puts dist_1 < dist_2 ? enemy_1 : enemy_2
 end
